923441f5744fa194a9061b2699be79c81def4a65
sun <sun007021@gmail.com>
2025-10-20 03:33:31 +0900

    feat(input): handle null input in calculation read, set "" to default

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public String readCalculationInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        return input != null ? input : "";
     }
 }
