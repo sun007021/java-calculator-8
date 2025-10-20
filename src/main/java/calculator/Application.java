07d9ae7095ee4ed705d154910bd3f2342c13c179
sun <sun007021@gmail.com>
2025-10-20 11:18:13 +0900

    feat(controller): integrate InputParser for test

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,13 @@
 package calculator;
 
+import calculator.domain.parser.InputParser;
+
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        CalculatorController calculatorController = new CalculatorController(inputView, outputView);
+        InputParser inputParser = new InputParser();
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView, inputParser);
         calculatorController.run();
     }
 }
--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,16 +1,21 @@
 package calculator;
 
+import calculator.domain.parser.InputParser;
+
 public class CalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final InputParser inputParser;
 
-    public CalculatorController(InputView inputView, OutputView outputView) {
+    public CalculatorController(InputView inputView, OutputView outputView, InputParser inputParser) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.inputParser = inputParser;
     }
 
     public void run() {
         String input = inputView.readCalculationInput();
+        inputParser.parse(input);
         outputView.printCalculationOutput(0);
     }
 }
