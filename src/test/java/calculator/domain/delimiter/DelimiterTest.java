417aa0822b713e85f753078b4d93b5df0500aa60
sun <sun007021@gmail.com>
2025-10-20 15:55:34 +0900

    test(delimiter): add unit test for creating Delimiter with numeric input

--- src/test/java/calculator/domain/delimiter/DelimiterTest.java
+++ src/test/java/calculator/domain/delimiter/DelimiterTest.java
@@ -54,4 +54,13 @@ class DelimiterTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1개의 문자");
     }
+
+    @Test
+    @DisplayName("숫자로 Delimiter를 생성하면 예외가 발생한다")
+    void createWithDigit() {
+        // given & when & then
+        assertThatThrownBy(() -> new Delimiter("5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구분자는 숫자가 될 수 없습니다");
+    }
 }
