1feacecb76a116b3f59c269248eada4726c95726
sun <sun007021@gmail.com>
2025-10-20 15:03:55 +0900

    feat(numbers): add Numbers class for handling a list of positive integers and translate string to integer

---
+++ src/main/java/calculator/domain/Numbers.java
@@ -0,0 +1,31 @@
+package calculator.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Numbers {
+    private final List<PositiveInteger> values;
+
+    public Numbers(List<PositiveInteger> values) {
+        this.values = new ArrayList<>(values);
+    }
+
+    public static Numbers from(String[] tokens) {
+        if (tokens.length == 0) {
+            return empty();
+        }
+
+        List<PositiveInteger> numbers = Arrays.stream(tokens)
+                .map(String::trim)
+                .map(PositiveInteger::from)
+                .collect(Collectors.toList());
+
+        return new Numbers(numbers);
+    }
+
+    public static Numbers empty() {
+        return new Numbers(new ArrayList<>());
+    }
+}
